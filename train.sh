8c653e29bc719d1dc019033d051cc61ccf6f6b91
swaubhik <p22cse100@cit.ac.in>
2024-05-22 14:19:11 +0530

    Add script to train the model using Undreamt

---
+++ train.sh
@@ -0,0 +1,38 @@
+# conda activate undreamt
+SRC=eng
+TGT=brx
+
+DATA_PATH=$PWD/dataset/eng-brx
+
+SRC_TOK=$DATA_PATH/train.tok.$SRC
+TGT_TOK=$DATA_PATH/train.tok.$TGT
+
+VALID_SRC=/home/bodoai/experiments/unsupervised/exp1/UnsupervisedMT/NMT/en-gb-data/para/dev/dev.en.tok
+VALID_TGT=/home/bodoai/experiments/unsupervised/exp1/UnsupervisedMT/NMT/en-gb-data/para/dev/dev.gb.tok
+
+UMT_PATH=$PWD
+TOOLS_PATH=$PWD/tools
+
+SRC_EMBEDDINGS=$DATA_PATH/eng.map
+TGT_EMBEDDINGS=$DATA_PATH/brx.map
+
+UNDREAMT_PATH=$TOOLS_PATH/undreamt
+mkdir -p data/models
+mkdir -p data/validation_output
+# train the model
+CUDA_VISIBLE_DEVICES=1,2 python tools/undreamt/train.py \
+    --batch 50 \
+    --src $SRC_TOK \
+    --trg $TGT_TOK \
+    --src_embeddings $SRC_EMBEDDINGS \
+    --trg_embeddings $TGT_EMBEDDINGS \
+    --save data/models/undreamt_model \
+    --save en_brx \
+    --cuda \
+    --validation $VALID_SRC $VALID_TGT \
+    --validation_output data/validation_output/undreamt_model.valid \
+    --save_interval 100000 \
+  
+# save tmux log to a file
+echo "Training completed. Saving log to train.log"
+tmux capture-pane -t 0 -p -S -1000000 > train.log
