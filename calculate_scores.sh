8ad2fb1d2587c06a62586924bac003af4e69f9e7
swaubhik <p22cse100@cit.ac.in>
2024-05-23 08:43:14 +0530

    chore: Update calculate_scores.sh script to use dataset-specific paths for validation

--- calculate_scores.sh
+++ calculate_scores.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # swaubhik
 
-ref_file=/home/bodoai/experiments/unsupervised/exp1/UnsupervisedMT/NMT/en-gb-data/para/dev/dev.en.tok
-pred_file=/home/bodoai/experiments/unsupervised/EN-BRX-UnsupNMT/data/validation_output/undreamt_model.valid.src2trg.0.8000.txt
+# ref_file=/home/bodoai/experiments/unsupervised/EN-BRX-UnsupNMT/dataset/eng-brx/dev.tok.brx
+pred_file=/home/bodoai/experiments/unsupervised/EN-BRX-UnsupNMT/data/validation_output/undreamt_model.valid.src2trg.0.6000.txt
 
-head -n 5 $ref_file
-head -n 5 $pred_file
+# head -n 5 $ref_file
+# head -n 5 $pred_file
+# echo "Calculating BLEU scores for the model"
+# sacrebleu $ref_file < $pred_file -m bleu chrf
+
+VALID_SRC=/home/bodoai/experiments/unsupervised/EN-BRX-UnsupNMT/dataset/eng-brx/dev.tok.eng
+VALID_TGT=/home/bodoai/experiments/unsupervised/EN-BRX-UnsupNMT/dataset/eng-brx/dev.tok.brx
+
 echo "Calculating BLEU scores for the model"
-sacrebleu $ref_file < $pred_file -m bleu chrf
-
-VALID_SRC=/home/bodoai/experiments/unsupervised/exp1/UnsupervisedMT/NMT/en-gb-data/para/dev/dev.en.tok
-VALID_TGT=/home/bodoai/experiments/unsupervised/exp1/UnsupervisedMT/NMT/en-gb-data/para/dev/dev.gb.tok
-
-echo "Calculating BLEU scores for the model"
-sacrebleu $VALID_SRC < $pred_file -m bleu chrf
+sacrebleu $VALID_TGT < $pred_file -m bleu chrf --force
